0xkobe/xchainjs-lib

commit 70487b11d033693dd726735ffbfd5a0162607ae3

Changes to support Ledger ETH - part 4 (#582)
- [x] Add optional `signer` params to `client.approve`
- [x] Add optional `signer` params to `client.transfer`
- [x] Remove `walletIndex` parameter from `client.estimateApprove` in favour of `fromAddress`
- [x] Latest `ethers@5.6.6`
- [x] Fix fee handling 516af1e
- [x] Bump `xchain-etherum@0.25.0-alpha.1

packages/xchain-ethereum/src/client.ts
@@ -6,6 +6,7 @@ import {
   BaseXChainClient,
   DepositParams,
   FeeOption,
+  FeeRates,
   FeeType,
   Fees,
   Network,
@@ -29,7 +30,7 @@ import {
   delay,
   getInboundDetails,
 } from '@xchainjs/xchain-util'
-import { BigNumber, Wallet, ethers } from 'ethers'
+import { BigNumber, Signer, Wallet, ethers } from 'ethers'
 import { HDNode, parseUnits, toUtf8Bytes } from 'ethers/lib/utils'
 
 import { LOWER_FEE_BOUND, UPPER_FEE_BOUND } from './const'
@@ -88,6 +89,8 @@ export interface EthereumClient {
   // `getFees` of `BaseXChainClient` needs to be overridden
   getFees(params: TxParams): Promise<Fees>
   getWallet(walletIndex?: number): ethers.Wallet
+  getProvider(): Provider
+  getEtherscanProvider(): EtherscanProvider
 }
 
 export type EthereumClientParams = XChainClientParams & {
@@ -198,6 +201,7 @@ export default class Client extends BaseXChainClient implements XChainClient, Et
     }
     return new Wallet(this.hdNode.derivePath(this.getFullDerivationPath(walletIndex))).connect(this.getProvider())
   }
+
   setupProviders(): void {
     if (this.infuraCreds) {
       // Infura provider takes either a string of project id
@@ -640,7 +644,7 @@ export default class Client extends BaseXChainClient implements XChainClient, Et
    *
    * @param {Address} contractAddress The contract address.
    * @param {Address} spenderAddress The spender address.
-   * @param {Address} fromAddress The address a transaction is send from.
+   * @param {signer} Signer (optional) The address a transaction is send from. If not set, signer will be defined based on `walletIndex`
    * @param {feeOptionKey} FeeOption Fee option (optional)
    * @param {BaseAmount} amount The amount of token. By default, it will be unlimited token allowance. (optional)
    * @param {number} walletIndex (optional) HD wallet index
@@ -653,9 +657,10 @@ export default class Client extends BaseXChainClient implements XChainClient, Et
     feeOptionKey: feeOption = FeeOption.Fastest,
     amount,
     walletIndex = 0,
+    signer: txSigner,
     gasLimitFallback,
   }: ApproveParams): Promise<TransactionResponse> {
-    const gasPrice = BigNumber.from(
+    const gasPrice: BigNumber = BigNumber.from(
       (
         await this.estimateGasPrices()
           .then((prices) => prices[feeOption])
@@ -665,25 +670,42 @@ export default class Client extends BaseXChainClient implements XChainClient, Et
         .toFixed(),
     )
 
-    const fromAddress = this.getAddress(walletIndex)
+    const signer = txSigner || this.getWallet(walletIndex)
 
-    const gasLimit = await this.estimateApprove({
+    const fromAddress = await signer.getAddress()
+
+    const gasLimit: BigNumber = await this.estimateApprove({
       spenderAddress,
       contractAddress,
-      walletIndex,
+      fromAddress,
       amount,
-    }).catch(() => BigNumber.from(gasLimitFallback))
+    }).catch((error) => {
+      if (gasLimitFallback) {
+        return BigNumber.from(gasLimitFallback)
+      }
 
-    const txAmount: BigNumber = getApprovalAmount(amount)
+      throw Error(`Could not estimate gas to send approve transaction ${error}`)
+    })
+
     checkFeeBounds(this.feeBounds, gasPrice.toNumber())
 
-    return await this.call<TransactionResponse>({
-      signer: this.getWallet(walletIndex),
-      contractAddress,
-      abi: erc20ABI,
-      funcName: 'approve',
-      funcParams: [spenderAddress, txAmount, { from: fromAddress, gasPrice, gasLimit }],
+    const valueToApprove: BigNumber = getApprovalAmount(amount)
+
+    const contract = new ethers.Contract(contractAddress, erc20ABI, this.getProvider())
+
+    const unsignedTx: ethers.PopulatedTransaction /* as same as ethers.TransactionResponse expected by `sendTransaction` */ = await contract.populateTransaction.approve(
+      spenderAddress,
+      valueToApprove,
+    )
+
+    const result = await signer.sendTransaction({
+      ...unsignedTx,
+      from: fromAddress,
+      gasPrice,
+      gasLimit,
     })
+
+    return result
   }
 
   /**
@@ -691,13 +713,13 @@ export default class Client extends BaseXChainClient implements XChainClient, Et
    *
    * @param {Address} contractAddress The contract address.
    * @param {Address} spenderAddress The spender address.
-   * @param {Address} fromAddress The spender address.
+   * @param {Address} fromAddress The address the approve transaction is sent from.
    * @param {BaseAmount} amount The amount of token. By default, it will be unlimited token allowance. (optional)
    *
    * @returns {BigNumber} Estimated gas
    */
   async estimateApprove({
-    walletIndex = 0,
+    fromAddress,
     contractAddress,
     spenderAddress,
     amount,
@@ -706,16 +728,17 @@ export default class Client extends BaseXChainClient implements XChainClient, Et
       provider: this.getProvider(),
       contractAddress,
       spenderAddress,
-      fromAddress: this.getAddress(walletIndex),
+      fromAddress,
       abi: erc20ABI,
       amount,
     })
   }
 
   /**
-   * Transfer ETH.
+   * Transfers ETH or ERC20 token
    *
    * @param {TxParams} params The transfer options.
+   * @param {signer} Signer (optional) The address a transaction is send from. If not set, signer will be defined based on `walletIndex`
    * @param {feeOptionKey} FeeOption Fee option (optional)
    * @param {gasPrice} BaseAmount Gas price (optional)
    * @param {gasLimit} BigNumber Gas limit (optional)
@@ -726,6 +749,7 @@ export default class Client extends BaseXChainClient implements XChainClient, Et
    */
   async transfer({
     walletIndex = 0,
+    signer: txSigner,
     asset,
     memo,
     amount,
@@ -734,6 +758,7 @@ export default class Client extends BaseXChainClient implements XChainClient, Et
     gasPrice,
     gasLimit,
   }: TxParams & {
+    signer?: Signer
     feeOptionKey?: FeeOption
     gasPrice?: BaseAmount
     gasLimit?: BigNumber
@@ -758,7 +783,7 @@ export default class Client extends BaseXChainClient implements XChainClient, Et
 
     // override `overrides` if `feeOption` is provided
     if (feeOption) {
-      const gasPrice = await this.estimateGasPrices()
+      const gasPrice: BaseAmount = await this.estimateGasPrices()
         .then((prices) => prices[feeOption])
         .catch(() => getDefaultGasPrices()[feeOption])
       const gasLimit = await this.estimateGasLimit({ asset, recipient, amount, memo }).catch(() => defaultGasLimit)
@@ -769,9 +794,12 @@ export default class Client extends BaseXChainClient implements XChainClient, Et
       }
     }
 
-    checkFeeBounds(this.feeBounds, BigNumber.from(overrides.gasPrice).toNumber())
+    // FIXME (xchain-devs): `gasPrice` can be `undefined, but needs to be set to run `checkFeeBounds`
+    if (overrides.gasPrice) {
+      checkFeeBounds(this.feeBounds, BigNumber.from(overrides.gasPrice).toNumber())
+    }
 
-    const signer = this.getWallet(walletIndex)
+    const signer = txSigner || this.getWallet(walletIndex)
 
     let txResult: TransactionResponse
     if (assetAddress && !isETHAddress) {
@@ -810,11 +838,13 @@ export default class Client extends BaseXChainClient implements XChainClient, Et
       // Note: `rates` are in `gwei`
       // @see https://gitlab.com/thorchain/thornode/-/blob/develop/x/thorchain/querier.go#L416-420
       // To have all values in `BaseAmount`, they needs to be converted into `wei` (1 gwei = 1,000,000,000 wei = 1e9)
-      const ratesInGwei = standardFeeRates(await this.getFeeRateFromThorchain())
+      const ratesInGwei: FeeRates = standardFeeRates(await this.getFeeRateFromThorchain())
+      // Note 2:
+      // `Fast` + `Fastest` needs to be increased by 2x or 3x (similar to `utils.estimateDefaultFeesWithGasPricesAndLimits`)
       return {
         [FeeOption.Average]: baseAmount(ratesInGwei[FeeOption.Average] * 10 ** 9, ETH_DECIMAL),
-        [FeeOption.Fast]: baseAmount(ratesInGwei[FeeOption.Fast] * 10 ** 9, ETH_DECIMAL),
-        [FeeOption.Fastest]: baseAmount(ratesInGwei[FeeOption.Fastest] * 10 ** 9, ETH_DECIMAL),
+        [FeeOption.Fast]: baseAmount(ratesInGwei[FeeOption.Fast] * 10 ** 9, ETH_DECIMAL).times(2),
+        [FeeOption.Fastest]: baseAmount(ratesInGwei[FeeOption.Fastest] * 10 ** 9, ETH_DECIMAL).times(3),
       }
     } catch (error) {}
     //should only get here if thor fails

packages/xchain-ethereum/__tests__/client.mainnet.test.ts
@@ -29,7 +29,7 @@ describe('Client Test', () => {
 
   it('derive path correctly with bip44', () => {
     const ethClient = new Client({
-      network: 'mainnet' as Network,
+      network: Network.Mainnet,
       phrase,
       ethplorerUrl,
     })
@@ -51,8 +51,8 @@ describe('Client Test', () => {
 
     const { fast, fastest, average } = await ethClient.estimateGasPrices()
 
-    expect(fast.amount().toString()).toEqual('30000000000')
-    expect(fastest.amount().toString()).toEqual('150000000000')
+    expect(fast.amount().toString()).toEqual('60000000000')
+    expect(fastest.amount().toString()).toEqual('450000000000')
     expect(average.amount().toString()).toEqual('15000000000')
   })
 

packages/xchain-ethereum/__tests__/client.testnet.test.ts
@@ -229,8 +229,8 @@ describe('Client Test', () => {
 
     const { fast, fastest, average } = await ethClient.estimateGasPrices()
 
-    expect(fast.amount().toString()).toEqual('30000000000')
-    expect(fastest.amount().toString()).toEqual('150000000000')
+    expect(fast.amount().toString()).toEqual('60000000000')
+    expect(fastest.amount().toString()).toEqual('450000000000')
     expect(average.amount().toString()).toEqual('15000000000')
   })
 
@@ -454,7 +454,7 @@ describe('Client Test', () => {
       ropstenInfuraUrl,
       ropstenAlchemyUrl,
       'eth_sendRawTransaction',
-      '0x4479b2af29590d5ad1b591ddfbb479dba37a5857c2a250b41c16bb2cecb7d08c',
+      '0xfa8f7c124948dbb47f300f56d26a9810217a932db5a8eae2b9d68809e56b3937',
     )
     mock_thornode_inbound_addresses_success(thornodeApiUrl, [
       {
@@ -486,7 +486,7 @@ describe('Client Test', () => {
       gasLimit: gasFee.gasLimit,
       gasPrice: gasFee.gasPrices.fastest,
     })
-    expect(txHash).toEqual('0x4479b2af29590d5ad1b591ddfbb479dba37a5857c2a250b41c16bb2cecb7d08c')
+    expect(txHash).toEqual('0xfa8f7c124948dbb47f300f56d26a9810217a932db5a8eae2b9d68809e56b3937')
   })
 
   it('estimate gas for eth transfer', async () => {
@@ -540,7 +540,7 @@ describe('Client Test', () => {
       ropstenInfuraUrl,
       ropstenAlchemyUrl,
       'eth_sendRawTransaction',
-      '0xea328780f0558b0bbf34baa142703957122678f5a5b9a0696102cff41a5d2682',
+      '0x92f7a7ecc80b955647988b705ad6a3607044226b64f2ce6a7ef2753296692a5b',
     )
 
     const gasEstimate = await ethClient.estimateFeesWithGasPricesAndLimits({
@@ -562,7 +562,7 @@ describe('Client Test', () => {
       phrase,
     })
 
-    beforeEach(() => {
+    it('approved', async () => {
       mock_all_api(etherscanUrl, ropstenInfuraUrl, ropstenAlchemyUrl, 'eth_blockNumber', '0xa7cac8')
       mock_all_api(etherscanUrl, ropstenInfuraUrl, ropstenAlchemyUrl, 'eth_getTransactionCount', '0x0')
       mock_all_api(etherscanUrl, ropstenInfuraUrl, ropstenAlchemyUrl, 'eth_gasPrice', '0x5969ec91')
@@ -573,13 +573,7 @@ describe('Client Test', () => {
         'eth_call',
         '0x0000000000000000000000000000000000000000000000000000000000000064', // 100
       )
-    })
 
-    afterEach(async () => {
-      nock.cleanAll()
-    })
-
-    it('approved', async () => {
       const result = await client.isApproved({
         contractAddress,
         spenderAddress,
@@ -590,6 +584,17 @@ describe('Client Test', () => {
     })
 
     it('not approved', async () => {
+      mock_all_api(etherscanUrl, ropstenInfuraUrl, ropstenAlchemyUrl, 'eth_blockNumber', '0xa7cac8')
+      mock_all_api(etherscanUrl, ropstenInfuraUrl, ropstenAlchemyUrl, 'eth_getTransactionCount', '0x0')
+      mock_all_api(etherscanUrl, ropstenInfuraUrl, ropstenAlchemyUrl, 'eth_gasPrice', '0x5969ec91')
+      mock_all_api(
+        etherscanUrl,
+        ropstenInfuraUrl,
+        ropstenAlchemyUrl,
+        'eth_call',
+        '0x0000000000000000000000000000000000000000000000000000000000000064', // 100
+      )
+
       const result = await client.isApproved({
         contractAddress,
         spenderAddress,
@@ -609,10 +614,12 @@ describe('Client Test', () => {
     mock_all_api(etherscanUrl, ropstenInfuraUrl, ropstenAlchemyUrl, 'eth_blockNumber', '0x3c6de5')
     mock_all_api(etherscanUrl, ropstenInfuraUrl, ropstenAlchemyUrl, 'eth_getTransactionCount', '0x10')
     mock_all_api(etherscanUrl, ropstenInfuraUrl, ropstenAlchemyUrl, 'eth_gasPrice', '0xb2d05e00')
-    mock_all_api(etherscanUrl, ropstenInfuraUrl, ropstenAlchemyUrl, 'eth_estimateGas', '0x5208')
+    mock_all_api(etherscanUrl, ropstenInfuraUrl, ropstenAlchemyUrl, 'eth_estimateGas', '0x5208') // 2100
+
+    const fromAddress = ethClient.getAddress(0)
 
     const gasLimit = await ethClient.estimateApprove({
-      walletIndex: 0,
+      fromAddress,
       contractAddress: '0x8c2a90d36ec9f745c9b28b588cba5e2a978a1656',
       spenderAddress: '0xdac17f958d2ee523a2206206994597c13d831ec7',
       amount: baseAmount(100, ETH_DECIMAL),
@@ -637,18 +644,21 @@ describe('Client Test', () => {
       ropstenInfuraUrl,
       ropstenAlchemyUrl,
       'eth_sendRawTransaction',
-      '0x06784c7a4652148d55d83002d967c2d0dab9447425f60b69d53cc79e15a17c2f',
+      '0x9df14ad5cf1a14d625cc6a6f2b5af4b410183387f85d9109985d7779ea07c869',
     )
+
+    const contractAddress = '0xA3910454bF2Cb59b8B3a401589A3bAcC5cA42306' // USDT
+    const spenderAddress = '0xeB005a0aa5027F66c8D195C77f7B01324C48501C' // router
+
     const tx = await ethClient.approve({
       walletIndex: 0,
-      contractAddress: '0xd15ffaef3112460bf3bcd81087fcbbce394e2ae7',
-      spenderAddress: '0x8c2a90d36ec9f745c9b28b588cba5e2a978a1656',
-      feeOptionKey: 'fastest' as FeeOption,
+      contractAddress,
+      spenderAddress,
+      feeOptionKey: FeeOption.Fastest,
       amount: baseAmount(100, ETH_DECIMAL),
     })
-    expect(tx.hash).toEqual('0x06784c7a4652148d55d83002d967c2d0dab9447425f60b69d53cc79e15a17c2f')
+    expect(tx.hash).toEqual('0x9df14ad5cf1a14d625cc6a6f2b5af4b410183387f85d9109985d7779ea07c869')
   })
-
   it('estimate call', async () => {
     const ethClient = new Client({
       network: 'testnet' as Network,

packages/xchain-client/src/feeRates.ts
@@ -1,4 +1,4 @@
-import { FeeOption, FeeRate, FeeRates, FeeBounds } from './types'
+import { FeeBounds, FeeOption, FeeRate, FeeRates } from './types'
 
 export function singleFeeRate(rate: FeeRate): FeeRates {
   return Object.values(FeeOption).reduce<Partial<FeeRates>>((a, x) => ((a[x] = rate), a), {}) as FeeRates

packages/xchain-ethereum/__tests__/util.test.ts
@@ -32,6 +32,14 @@ import {
 } from '../src/utils'
 
 describe('ethereum/util', () => {
+  beforeEach(() => {
+    nock.disableNetConnect()
+  })
+
+  afterEach(() => {
+    nock.cleanAll()
+  })
+
   describe('xchainNetworkToEths', () => {
     it('should return mainnet ', () => {
       expect(xchainNetworkToEths('mainnet' as Network)).toEqual(EthNetwork.Main)
@@ -427,7 +435,6 @@ describe('ethereum/util', () => {
     })
 
     it('USDT - testnet', async () => {
-      nock.disableNetConnect()
       mock_etherscan_api(
         'https://api-ropsten.etherscan.io',
         'eth_call',
@@ -446,8 +453,6 @@ describe('ethereum/util', () => {
         provider,
       )
       expect(decimal).toEqual(6)
-
-      nock.cleanAll()
     })
   })
 
@@ -469,20 +474,12 @@ describe('ethereum/util', () => {
     const spenderAddress = '0xeB005a0aa5027F66c8D195C77f7B01324C48501C' // router
     const provider = new providers.EtherscanProvider(xchainNetworkToEths(Network.Testnet))
 
-    beforeEach(() => {
-      nock.disableNetConnect()
+    it('is approved', async () => {
       mock_etherscan_api(
         'https://api-ropsten.etherscan.io',
         'eth_call',
         '0x0000000000000000000000000000000000000000000000000000000000000064', // 100
       )
-    })
-
-    afterEach(() => {
-      nock.cleanAll()
-    })
-
-    it('is approved', async () => {
       const result = await isApproved({
         provider,
         fromAddress,
@@ -494,6 +491,11 @@ describe('ethereum/util', () => {
     })
 
     it('is not approved', async () => {
+      mock_etherscan_api(
+        'https://api-ropsten.etherscan.io',
+        'eth_call',
+        '0x0000000000000000000000000000000000000000000000000000000000000064', // 100
+      )
       const result = await isApproved({
         provider,
         fromAddress,
@@ -528,8 +530,6 @@ describe('ethereum/util', () => {
       })
 
       expect(gas.toString()).toEqual('21000')
-
-      nock.cleanAll()
     })
   })
 
@@ -552,8 +552,6 @@ describe('ethereum/util', () => {
       })
 
       expect(gas.toString()).toEqual('21000')
-
-      nock.cleanAll()
     })
   })
 
@@ -576,7 +574,6 @@ describe('ethereum/util', () => {
 
   describe('call', () => {
     it('`decimals`', async () => {
-      nock.disableNetConnect()
       mock_etherscan_api(
         'https://api-ropsten.etherscan.io',
         'eth_call',
@@ -592,8 +589,6 @@ describe('ethereum/util', () => {
         funcName: 'decimals',
       })
       expect(ethers.BigNumber.from(decimal).toString()).toEqual('6')
-
-      nock.cleanAll()
     })
   })
 })

packages/xchain-ethereum/src/index.ts
@@ -1,13 +1,22 @@
 export * from './types'
 export * from './client'
 export {
+  call,
+  estimateApprove,
+  estimateCall,
   estimateDefaultFeesWithGasPricesAndLimits,
+  getApprovalAmount,
   getDecimal,
   getDefaultFees,
+  getDefaultGasPrices,
   getTokenAddress,
   getPrefix,
   getFee,
+  isApproved,
+  strip0x,
   validateAddress,
   ETHAddress,
   ETH_DECIMAL,
+  ETHPLORER_FREEKEY,
+  MAX_APPROVAL,
 } from './utils'

packages/xchain-ethereum/src/types/client-types.ts
@@ -33,6 +33,7 @@ export type FeesWithGasPricesAndLimits = { fees: Fees; gasPrices: GasPrices; gas
 
 export type ApproveParams = {
   walletIndex?: number
+  signer?: Signer
   contractAddress: Address
   spenderAddress: Address
   feeOptionKey?: FeeOption
@@ -41,7 +42,12 @@ export type ApproveParams = {
   gasLimitFallback?: ethers.BigNumberish
 }
 
-export type EstimateApproveParams = Omit<ApproveParams, 'feeOptionKey' | 'gasLimitFallback'>
+export type EstimateApproveParams = {
+  contractAddress: Address
+  spenderAddress: Address
+  fromAddress: Address
+  amount?: BaseAmount
+}
 
 export type IsApprovedParams = {
   walletIndex?: number

commit 1286a889640b5948e5b83bd842431de981fcc131

[xchain-ethereum] Fix `estimateGasPrices` (#591)
No need to multiply fee rates. It's already done in `standardFeeRates` of `xchain-client` ([code](https://github.com/xchainjs/xchainjs-lib/blob/723608214ce512deab80cdb64d64e74b859fefee/packages/xchain-client/src/feeRates.ts#L7-L13)). This bug was  introduced in #582 as part of `xchain-ethereum@0.25.alpha.1` _(my bad)_.

Bump `xchain-ethereum@0.25.alpha.3`

packages/xchain-ethereum/src/client.ts
@@ -836,12 +836,10 @@ export default class Client extends BaseXChainClient implements XChainClient, Et
       // @see https://gitlab.com/thorchain/thornode/-/blob/develop/x/thorchain/querier.go#L416-420
       // To have all values in `BaseAmount`, they needs to be converted into `wei` (1 gwei = 1,000,000,000 wei = 1e9)
       const ratesInGwei: FeeRates = standardFeeRates(await this.getFeeRateFromThorchain())
-      // Note 2:
-      // `Fast` + `Fastest` needs to be increased by 2x or 3x (similar to `utils.estimateDefaultFeesWithGasPricesAndLimits`)
       return {
         [FeeOption.Average]: baseAmount(ratesInGwei[FeeOption.Average] * 10 ** 9, ETH_DECIMAL),
-        [FeeOption.Fast]: baseAmount(ratesInGwei[FeeOption.Fast] * 10 ** 9, ETH_DECIMAL).times(2),
-        [FeeOption.Fastest]: baseAmount(ratesInGwei[FeeOption.Fastest] * 10 ** 9, ETH_DECIMAL).times(3),
+        [FeeOption.Fast]: baseAmount(ratesInGwei[FeeOption.Fast] * 10 ** 9, ETH_DECIMAL),
+        [FeeOption.Fastest]: baseAmount(ratesInGwei[FeeOption.Fastest] * 10 ** 9, ETH_DECIMAL),
       }
     } catch (error) {}
     //should only get here if thor fails

packages/xchain-ethereum/__tests__/client.mainnet.test.ts
@@ -51,8 +51,8 @@ describe('Client Test', () => {
 
     const { fast, fastest, average } = await ethClient.estimateGasPrices()
 
-    expect(fast.amount().toString()).toEqual('60000000000')
-    expect(fastest.amount().toString()).toEqual('450000000000')
+    expect(fast.amount().toString()).toEqual('30000000000')
+    expect(fastest.amount().toString()).toEqual('150000000000')
     expect(average.amount().toString()).toEqual('15000000000')
   })
 

packages/xchain-ethereum/__tests__/client.testnet.test.ts
@@ -229,8 +229,8 @@ describe('Client Test', () => {
 
     const { fast, fastest, average } = await ethClient.estimateGasPrices()
 
-    expect(fast.amount().toString()).toEqual('60000000000')
-    expect(fastest.amount().toString()).toEqual('450000000000')
+    expect(fast.amount().toString()).toEqual('30000000000')
+    expect(fastest.amount().toString()).toEqual('150000000000')
     expect(average.amount().toString()).toEqual('15000000000')
   })
 
@@ -454,7 +454,7 @@ describe('Client Test', () => {
       ropstenInfuraUrl,
       ropstenAlchemyUrl,
       'eth_sendRawTransaction',
-      '0xfa8f7c124948dbb47f300f56d26a9810217a932db5a8eae2b9d68809e56b3937',
+      '0x4479b2af29590d5ad1b591ddfbb479dba37a5857c2a250b41c16bb2cecb7d08c',
     )
     mock_thornode_inbound_addresses_success(thornodeApiUrl, [
       {
@@ -486,7 +486,7 @@ describe('Client Test', () => {
       gasLimit: gasFee.gasLimit,
       gasPrice: gasFee.gasPrices.fastest,
     })
-    expect(txHash).toEqual('0xfa8f7c124948dbb47f300f56d26a9810217a932db5a8eae2b9d68809e56b3937')
+    expect(txHash).toEqual('0x4479b2af29590d5ad1b591ddfbb479dba37a5857c2a250b41c16bb2cecb7d08c')
   })
 
   it('estimate gas for eth transfer', async () => {